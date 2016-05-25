fb24d08be78042c665df11c8a33e539030e62b20
kumakurakoutarou <koutaro@kumakurakoutarou-no-MacBook-Ai.local>
2016-05-25 20:27:30 +0900

    test

--- count/ViewController.swift
+++ count/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var label:UILabel!
+    var number :Int! = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +21,32 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func plus(){
+        number = number + 1
+        label.text = String(number)
+        if number >= 10 {
+            label.textColor = UIColor.redColor()
+        }else if number <= -10 {
+            label.textColor = UIColor.blueColor()
+        }else {
+            label.textColor = UIColor.blackColor()
+        }
+    }
+    
+    
+    @IBAction func minus(){
+        number = number - 1
+        label.text = String(number)
+        if number >= 10 {
+            label.textColor = UIColor.redColor()
+        }else if number <= -10 {
+            label.textColor = UIColor.blueColor()
+        }else {
+            label.textColor = UIColor.blackColor()
+        }
     }
 
 
